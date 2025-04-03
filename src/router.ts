bcf02c120b5c100eb38dd2e33dd1e4bcb9ac5836
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-04-03 15:06:27 -0300

    Add error middleware and fix error handling as described in bit.ly/3Rww26S and https://bit.ly/4c9OxYg (clean solution based in https://github.com/brunopk/nodejs-server/commit/da1a301dcca3fdf7558ee4bcca911fa0ea202b88)

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,19 @@
-import express from 'express';
-import 'express-async-errors';
+import express, {Request, Response, NextFunction, RequestHandler} from 'express';
 import { getCategories, getSubCategories, getGroups, getAccounts } from './handlers';
 
 const app = express();
 
-app.get('/categories', getCategories);
+const asyncHandler = (func: RequestHandler) => (req: Request, res: Response, next: NextFunction) => {
+  Promise.resolve(func(req, res, next))
+    .catch(next)
+}
 
-app.get('/subcategories', getSubCategories);
+app.get('/categories', asyncHandler(getCategories));
 
-app.get('/groups', getGroups);
+app.get('/subcategories', asyncHandler(getSubCategories));
 
-app.get('/accounts', getAccounts);
+app.get('/groups', asyncHandler(getGroups));
+
+app.get('/accounts',asyncHandler(getAccounts));
 
 export default app;
