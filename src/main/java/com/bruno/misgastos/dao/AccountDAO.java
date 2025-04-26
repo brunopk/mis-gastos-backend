917aad2c95f9bd144da0545b8b783f667c758c27
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-04-26 13:34:03 -0300

    Add TODO

--- src/main/java/com/bruno/misgastos/dao/AccountDAO.java
+++ src/main/java/com/bruno/misgastos/dao/AccountDAO.java
@@ -3,6 +3,8 @@ package com.bruno.misgastos.dao;
 import com.bruno.misgastos.dto.Account;
 import java.util.List;
 
+// TODO: fix account id as 0 for spends
+
 public interface AccountDAO {
     List<Account> list();
 }
