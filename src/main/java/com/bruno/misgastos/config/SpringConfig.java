320c171139cd340c13ab34e280b3a4ef0ddb17d9
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-04-27 20:39:51 -0300

    Disable CSRF for development

--- src/main/java/com/bruno/misgastos/config/SpringConfig.java
+++ src/main/java/com/bruno/misgastos/config/SpringConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+
+// TODO: configure CSRF for prod
 
 @Configuration
 public class SpringConfig {
@@ -14,6 +17,7 @@ public class SpringConfig {
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     return http
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .csrf(AbstractHttpConfigurer::disable)
         .build();
   }
 }
