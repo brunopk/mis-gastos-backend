b98b1ce845fa98f25aba7e2d928dd1d768fbd46e
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-04-28 21:21:14 -0300

    Fix security configurations for prod

--- src/main/java/com/bruno/misgastos/config/SpringConfig.java
+++ src/main/java/com/bruno/misgastos/config/SpringConfig.java
@@ -2,18 +2,14 @@ package com.bruno.misgastos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-// TODO: configure CSRF for prod
 
 @Configuration
 public class SpringConfig {
 
   @Bean
-  @Profile(value = "default")
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     return http
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
