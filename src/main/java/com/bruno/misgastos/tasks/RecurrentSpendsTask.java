8180c7e6c57be0981120830a8d5508c3fdf80d50
Bruno Piaggio <brunopiaggiok@gmail.com>
2025-06-26 15:26:34 -0300

    Add TODO

--- src/main/java/com/bruno/misgastos/tasks/RecurrentSpendsTask.java
+++ src/main/java/com/bruno/misgastos/tasks/RecurrentSpendsTask.java
@@ -8,7 +8,18 @@ import org.slf4j.LoggerFactory;
 
 // TODO: continue
 
-// TODO: if the scheduled task fails due to credentials error , the whole application should catch the error and finish
+// TODO: if the scheduled task fails due to credentials error , the whole application should catch
+// the error and finish
+
+// TODO: rename to RecurrentSpendTask
+
+// TODO: create a function like this ...
+// https://github.com/brunopk/mis-gastos/blob/90a9be15182955c033a31ff73db2aaa4298b4593/src/Utils.ts#L301C27-L301C61
+
+// TODO: create a spend (if type is automatic) like this ...
+// https://github.com/brunopk/mis-gastos/blob/90a9be15182955c033a31ff73db2aaa4298b4593/src/Main.ts#L81
+
+// TODO: if type is "automatic" completed should be set on "true"
 
 public class RecurrentSpendsTask extends AbstractTask {
 
