6538795e61242b7d53eff67c49cf34f98788c02b
Bruno Piaggio <brunopiaggiok@gmail.com>
2026-09-19 22:17:09 -0300

    Fix CORS configuration for local environment and session (cookies) policy

--- src/main/java/com/bruno/misgastos/config/SecurityConfig.java
+++ src/main/java/com/bruno/misgastos/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bruno.misgastos.config;
 
 import com.bruno.misgastos.OAuth2LoginSuccessHandler;
 import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,12 +23,28 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+/**
+ * Among other things, {@code oauth2Login} configuration provides an implementation for the GET /login endpoint.
+ * <br>
+ * <br>
+ * {@code IF_REQUIRED} is used because OAuth2 login relies on the HTTP session to persist the authenticated
+ * {@code SecurityContext}. It allows Spring Security to create the session required to store the authentication and
+ * reuse that same session for subsequent requests. {@code NEVER} is not suitable because Spring Security is prevented
+ * from creating a session. If n suitable session exists when the authenticated {@code SecurityContext} needs to be
+ * persisted, another component may create a new HTTP session later in the request lifecycle. That session receives a
+ * new {@code JSESSIONID} and does not necessarily contain the authenticated SecurityContext, causing subsequent
+ * requests to be associated with a different, unauthenticated session. {@code STATELESS} is not suitable because it
+ * explicitly prevents the HTTP session from being used to persist the authenticated {@code SecurityContext}.
+ */
 @Configuration
 public class SecurityConfig {
 
   /**
-   * Among other things, {@code oauth2Login} configuration provides an implementation for the GET /login endpoint.
+   * Filter chain configuration
    *
    * @param http Provided by Spring
    * @param oAuth2LoginSuccessHandler Used to map email to principal name (Google)
@@ -39,7 +56,40 @@ public class SecurityConfig {
   public SecurityFilterChain defaultSecurityFilterChain(
       HttpSecurity http, OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler) throws Exception {
     return http.csrf(AbstractHttpConfigurer::disable)
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+        .cors(Customizer.withDefaults())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+        .authorizeHttpRequests(
+            (authorizationManagerRequestMatcherRegistry) ->
+                authorizationManagerRequestMatcherRegistry
+                    .requestMatchers("/oauth2/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .oauth2Login(
+            oauth2LoginCustomizer ->
+                oauth2LoginCustomizer.successHandler(oAuth2LoginSuccessHandler))
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .build();
+  }
+
+  /**
+   * Filter chain configuration
+   *
+   * @param http Provided by Spring
+   * @param oAuth2LoginSuccessHandler Used to map email to principal name (Google)
+   * @return generated {@code SecurityFilterChain} bean
+   * @throws Exception .
+   */
+  @Bean
+  @Profile({"prod"})
+  public SecurityFilterChain prodSecurityFilterChain(
+      HttpSecurity http, OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler) throws Exception {
+    return http.csrf(
+            csrf ->
+                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    .ignoringRequestMatchers("/oauth2/token"))
+        .cors(Customizer.withDefaults())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
         .authorizeHttpRequests(
             (authorizationManagerRequestMatcherRegistry) ->
                 authorizationManagerRequestMatcherRegistry
@@ -52,32 +102,20 @@ public class SecurityConfig {
         .build();
   }
 
-  /**
-   * Among other things, {@code oauth2Login} configuration provides an implementation for the GET /login endpoint.
-   *
-   * @param http Provided by Spring
-   * @param oAuth2LoginSuccessHandler Used to map email to principal name (Google)
-   * @return generated {@code SecurityFilterChain} bean
-   * @throws Exception .
-   */
   @Bean
-  @Profile({"prod"})
-  public SecurityFilterChain prodSecurityFilterChain(HttpSecurity http, OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler) throws Exception {
-    return http.csrf(
-            csrf ->
-                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                    .ignoringRequestMatchers("/oauth2/token"))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER))
-        .authorizeHttpRequests(
-            (authorizationManagerRequestMatcherRegistry) ->
-                authorizationManagerRequestMatcherRegistry
-                    .requestMatchers("/oauth2/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
-        .oauth2Login(oauth2LoginCustomizer -> oauth2LoginCustomizer.successHandler(oAuth2LoginSuccessHandler))
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-        .build();
+  @Profile({"default", "local"})
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 
   /**
